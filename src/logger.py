f4757db45b8d5ad5e5700407fdbe4ec296542e11
Phantom-VK <vikramadityakhupse@gmail.com>
2025-06-19 13:59:44 +0530

    Added Project structure, Logging and Exception handling

---
+++ src/logger.py
@@ -0,0 +1,30 @@
+import logging
+import os
+import sys
+from datetime import datetime
+
+from src.exception import CustomException
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(), "logs", f"{datetime.now().strftime('%m_%d_%Y_%H_%M')}")
+os.makedirs(logs_path, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+logging.basicConfig(
+        handlers=[
+            logging.FileHandler(LOG_FILE_PATH),
+            logging.StreamHandler()  # Prints to console
+        ],
+        format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+        level=logging.INFO
+
+    )
+
+#Test
+# if __name__ == "__main__":
+#     try:
+#         a = 1 / 0
+#     except Exception as e:
+#         raise CustomException(e, sys)
+#     logging.info("Testing loger, logger has started")
