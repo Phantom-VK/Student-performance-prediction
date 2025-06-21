ea932b686198b93e5285bfb6589ce892f19aa791
Phantom-VK <vikramadityakhupse@gmail.com>
2025-06-21 13:15:27 +0530

    Fixed log folder path

--- src/logger.py
+++ src/logger.py
@@ -1,26 +1,24 @@
 import logging
 import os
 from datetime import datetime
-
-from src.exception import CustomException
+from project_config import PROJECT_ROOT
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(), "logs", f"{datetime.now().strftime('%m_%d_%Y_%H_%M')}")
+logs_path = os.path.join(PROJECT_ROOT, "logs", f"{datetime.now().strftime('%m_%d_%Y_%H_%M')}")
 os.makedirs(logs_path, exist_ok=True)
-
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
         handlers=[
             logging.FileHandler(LOG_FILE_PATH),
-            logging.StreamHandler()  # Pr  ints to console
+            logging.StreamHandler()
         ],
         format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
         level=logging.INFO
 
 )
 
-#Test
+# Test
 # if __name__ == "__main__":
 #     try:
 #         a = 1 / 0
